ae968ef5141769925eb50566ec47f32fe429822b
Gustavo <gugustavo137@gmail.com>
2024-06-04 23:56:45 -0300

    Using find method

--- 1143_longest_common_subsequence.py
+++ 1143_longest_common_subsequence.py
@@ -1,7 +1,7 @@
 def longestCommonSubsequence(text1, text2):
     sub_sequence_size = 0
     for c in text1:
-        print(c)
-    return 1
+        print( f"{c} encontrado na posicao {text2.find(c)}")
+    return 999
 
 print(longestCommonSubsequence("AXXBXXCXXD","ABCD"))
